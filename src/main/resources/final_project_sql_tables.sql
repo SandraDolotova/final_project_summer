09b1640234960a65dcb8e541479b105bbf7246ac
SandraDolotova <sandrafort@inbox.lv>
2021-08-11 15:56:10 +0300

    Initial commit - sql tables3

--- src/main/resources/final_project_sql_tables.sql
+++ src/main/resources/final_project_sql_tables.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS event_manager;
 use event_manager;
 
-DROP table user_validation;
 CREATE table IF NOT EXISTS user_validation ( -- owner can delete user
 id int auto_increment not null,
 login_name varchar (50) not null, -- only for login
@@ -16,11 +15,9 @@ CREATE table IF NOT EXISTS events (  -- customer or owner can add/delete/update
 event_id int auto_increment primary key  not null,
 event_name varchar(50) not null,
 dueDate date not null,
-location varchar (50) not null,
-distance_to_location int,
+location_name varchar (50) not null,
 guests_number int not null
 );
-
 
 CREATE table IF NOT EXISTS event_guest_list( -- customer can see guest list and add/delete/update
 guest_id int auto_increment not null,
@@ -32,21 +29,23 @@ foreign key(guest_id) references events (event_id)
 );
 
 CREATE table decor_list ( -- owner can add/delete/update
-item_id int auto_increment primary key not null,
-item_name varchar(50) not null, -- can see customer + owner
-item_qwt int not null, -- can see customer + owner
-item_price real, -- can see owner
-item_price_vat real AS (item_price * 1.21), -- can see customer + owner
-item_status varchar (50) not null -- can see owner
+decor_id int auto_increment primary key not null,
+decor_name varchar(50) not null, -- can see customer + owner
+decor_qwt int not null, -- can see customer + owner
+decor_price real, -- can see owner + update
+decor_price_vat real AS (decor_price * 1.21), -- can see customer + owner
+decor_status varchar (50) not null -- can see owner + update status
 );
 
-CREATE table chosen_decor( -- table for bill
+CREATE table bill ( -- table for bill will be completed from customer inputs
 bill_id int auto_increment not null,
 event_name varchar (50) not null,
-item_id int,
-item_name varchar(50) not null,
-item_price_vat real,
-item_qwt int not null,
-total_price real AS (item_qwt * item_price_vat),
+chosen_decor_id int,
+chosen_decor_name varchar(50) not null,
+chosen_decor_price_vat real,
+chosen_decor_qwt int not null,
+total_decor_price real AS (chosen_decor_qwt * chosen_decor_price_vat),
+transportation_costs real AS (total_decor_price * 1.15),
+total_bill real AS (total_decor_price + transportation_costs),
 foreign key(bill_id) references events (event_id)
 );
