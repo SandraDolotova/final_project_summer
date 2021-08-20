2b59726e8f5c092d9e1af08b77b9ca777d979b31
SandraDolotova <sandrafort@inbox.lv>
2021-08-20 14:55:50 +0300

    check user login name exists in db

---
+++ src/main/java/users/UserValidation.java
@@ -0,0 +1,27 @@
+package users;
+import Panels.WelcomePanel;
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class UserValidation extends WelcomePanel {
+    UserDBService userDBService = new UserDBService();
+    String userInputLoginName = loginField.getText();
+    String userInputPassword = passField.getText();
+
+    // to validate user input according to names and passwords in DB
+    public void login() throws SQLException {
+        if (!userDBService.userCheck().containsKey(userInputLoginName) || !userDBService.userCheck().containsValue(userInputPassword)) {
+            showMessageWarning();
+        } else if (userDBService.userCheck().containsKey(userInputLoginName) &&
+                userDBService.userCheck().containsValue(userInputPassword)) {
+            showMessagePositive();
+        }
+    }
+    private void showMessageWarning() {
+        JOptionPane.showMessageDialog(dialogFrame, "Incorrect input. Try again", "warning", JOptionPane.WARNING_MESSAGE);
+    }
+    private void showMessagePositive() {
+        JOptionPane.showMessageDialog(dialogFrame, "You are logged in successfully", "good", JOptionPane.PLAIN_MESSAGE);
+    }
+    private static JFrame dialogFrame;
+}
